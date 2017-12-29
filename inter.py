49845e9b7787da87950f73c300e5885fce1ab098
bjkim777 <scidakim@gmail.com>
2017-12-29 15:03:17 +0900

    intersection

---
+++ inter.py
@@ -0,0 +1,27 @@
+import sys
+
+
+class MkList():
+   def __init__(self)
+      
+
+def InterSection():
+ 
+
+def main(f):
+
+
+
+if __name__ == "__main__":
+   if len(sys.argv) == 2:
+      f = sys.argv[1]
+   else:
+      sys.stderr.write(
+"""Error! please input 1st parameter
+1st parameter is Data(csv)
+example : python decide-high-or-low.py [data]
+""")
+      sys.exit(1)
+
+   main(f)
+   sys.exit(0)
